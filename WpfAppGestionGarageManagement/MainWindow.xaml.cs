c1c9b37db2d2854af13c0ebf59bcfaf7023f1048
Anas-Emmanuel FAOUR <anas-emmanuel.faour@orange.fr>
2020-07-08 09:09:35 +0200

    create web service - Gestion Garage & Code First

---
+++ WpfAppGestionGarageManagement/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAppGestionGarageManagement.ServiceReference1;
+
+namespace WpfAppGestionGarageManagement
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private static List<Voiture> ListVoitures()
+        {
+            Service1Client cli = new Service1Client();
+            var res = cli.ListVoitures();
+            cli.Close();
+            return res.ToList();
+        }
+        private void DisV_Click(object sender, RoutedEventArgs e)
+        {
+            var _voitures = ListVoitures();
+            LBGarage.ItemsSource = _voitures;
+        }
+    }
+}
